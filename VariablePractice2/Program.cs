2c4d983321666965ec7a35a9e2ce7a91f3dd76fa
MBurnette4120 <mikeburnette@rocketmail.com>
2020-06-22 00:22:26 -0400

    Add project files.

---
+++ VariablePractice2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VariablePractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------------Exercise 1
+
+            //Name: Michael
+            //Favorite Color: Blue
+            //Favorite Animal: Walrus
+            //Favorite Band: The Beatles
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}, What is your favorite color?");
+            var colorName = Console.ReadLine();
+
+            Console.WriteLine($"{colorName} is an awesome color.  What is your favorite animal?");
+            var animalName = Console.ReadLine();
+
+            Console.WriteLine($"I love the {animalName}. Who is your favorite band?");
+            var bandName = Console.ReadLine();
+
+            Console.WriteLine($"Last week {userName} went to the store and bought some {animalName}.");
+            Console.WriteLine($"When I got home {userName} listened to some music by {bandName}");
+            Console.WriteLine($"I was so {colorName} this morning that I took a {animalName} this afternoon");
+            Console.WriteLine($"I wish {bandName} had a {animalName}, as I had a lot of {animalName} to do today");
+
+        }
+    }
+}
